03a904d50e4513d46d684a1282313ae287a4d958
hango <thuhango0204@gmail.com>
2024-08-16 17:00:00 +0700

    chore: update i18n - footer

--- src/services/i18n/en.ts
+++ src/services/i18n/en.ts
@@ -6,4 +6,7 @@ export default {
   "Experiences": "Experiences",
   "Projects": "Projects",
   "Contact me": "Contact me",
+  "Built and designed by": "Built and designed by",
+  "Copyright ©": "Copyright ©",
+  "All Rights Reserved": "All Rights Reserved",
 } satisfies Record<string, string>;
