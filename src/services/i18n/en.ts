aa83c7179c01e128de63fb44a6c2d51c69794196
hango <thuhango0204@gmail.com>
2024-10-03 18:55:29 +0700

    chore: update i18n

--- src/services/i18n/en.ts
+++ src/services/i18n/en.ts
@@ -12,4 +12,13 @@ export default {
   "Description":
     "Passionate about programming, striving to create applications that enhance user experience and solve real-world problems through technology. Enthusiastic about solving real-life problems and enjoys learning new technologies.",
   "Download CV": "Download CV",
+  "University":
+    "Posts and Telecommunications Institute of Technology",
+  "Major": "Major",
+  "Information Technology": "Information Technology",
+  "Software Technology": "Software Technology",
+  "GPA": "GPA",
+  "Transcript": "Transcript",
+  "Degree": "Degree",
+  "Engineer's degree": "Engineer's degree",
 } satisfies Record<string, string>;
--- src/services/i18n/vi.ts
+++ src/services/i18n/vi.ts
@@ -13,4 +13,12 @@ export default {
   "Description":
     "Đam mê lập trình, luôn nỗ lực tạo ra các ứng dụng nâng cao trải nghiệm người dùng và giải quyết các vấn đề thực tiễn thông qua công nghệ. Nhiệt huyết với việc giải quyết các vấn đề thực tế và yêu thích việc học hỏi các công nghệ mới.",
   "Download CV": "Tải CV",
+  "University": "Học viện Công nghệ Bưu chính Viễn thông",
+  "Major": "Ngành học",
+  "Information Technology": "Công nghệ Thông tin",
+  "Software Technology": "Công nghệ Phần mềm",
+  "GPA": "Điểm trung bình",
+  "Transcript": "Bảng điểm",
+  "Degree": "Bằng cấp",
+  "Engineer's degree": "Bằng Kỹ sư",
 } satisfies Record<string, string>;
