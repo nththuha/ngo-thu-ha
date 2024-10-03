364ad07f05e1d342ff753a520d3f2cab195ee336
hango <thuhango0204@gmail.com>
2024-10-03 14:37:55 +0700

    chore: update i18n

--- src/services/i18n/en.ts
+++ src/services/i18n/en.ts
@@ -9,4 +9,7 @@ export default {
   "Built and designed by": "Built and designed by",
   "Copyright ©": "Copyright ©",
   "All Rights Reserved": "All Rights Reserved",
+  "Description":
+    "Passionate about programming, striving to create applications that enhance user experience and solve real-world problems through technology. Enthusiastic about solving real-life problems and enjoys learning new technologies.",
+  "Download CV": "Download CV",
 } satisfies Record<string, string>;
--- src/services/i18n/vi.ts
+++ src/services/i18n/vi.ts
@@ -10,4 +10,7 @@ export default {
   "Built and designed by": "Được xây dựng và thiết kế bởi",
   "Copyright ©": "Bản quyền ©",
   "All Rights Reserved": "Đã được Bảo lưu",
+  "Description":
+    "Đam mê lập trình, luôn nỗ lực tạo ra các ứng dụng nâng cao trải nghiệm người dùng và giải quyết các vấn đề thực tiễn thông qua công nghệ. Nhiệt huyết với việc giải quyết các vấn đề thực tế và yêu thích việc học hỏi các công nghệ mới.",
+  "Download CV": "Tải CV",
 } satisfies Record<string, string>;
